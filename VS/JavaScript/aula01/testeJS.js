355dd83e4dfdcb89a09120f791747fb328607192
VictorDias2 <victorrdias2015@gmail.com>
2024-09-02 13:38:24 -0300

    PROA

---
+++ VS/JavaScript/aula01/testeJS.js
@@ -0,0 +1,20 @@
+function show_camada1(){
+    document.getElementById("camada1").style.visibility="visible";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="hidden";
+}
+function show_camada2(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="visible";
+    document.getElementById("camada3").style.visibility="hidden";
+}
+function show_camada3(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="visible";
+}
+function sumir(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="hidden";
+}
