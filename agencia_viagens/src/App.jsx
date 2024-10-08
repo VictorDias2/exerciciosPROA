f3db44b0f22932f3855ac95b4f17f2720a05349e
VictorDias2 <victorrdias2015@gmail.com>
2024-10-08 12:46:09 -0300

    .

--- agencia_viagens/src/App.jsx
+++ agencia_viagens/src/App.jsx
@@ -1,6 +1,11 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './assets/Componentes/Header'
 import Home from './assets/Componentes/Home'
 import Rodape from './assets/Componentes/Rodape'
+import Escocia from './assets/Componentes/Escocia'
+import Muralha from './assets/Componentes/Muralha'
+import Grand from './assets/Componentes/Grand'
+import Aruba from './assets/Componentes/Aruba'
 
 import './App.css'
 
@@ -9,9 +14,17 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Home />
-      <Rodape />
+      <Router>
+          <Header />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/Escocia' element={<Escocia />} />
+              <Route path='/Grand' element={<Grand />} />
+              <Route path='/Muralhas' element={<Muralha />} />
+              <Route path='/Aruba' element={<Aruba />} />
+          </Routes>
+          <Rodape />
+      </Router>
     </>
   )
 }
--- agencia_viagens/src/assets/Componentes/Header.jsx
+++ agencia_viagens/src/assets/Componentes/Header.jsx
@@ -2,21 +2,23 @@
 import Logo from '../imagens/viagens.jpg'
 import Lupa from '../imagens/lupa.png'
 import styles from '../css/Header.module.css'
+import{Link} from 'react-router-dom'
 
 function Header(){
 
     return(
 
         <header className={styles.content}>
-            <img src={Logo} alt="Logo" className={styles.logo} />
+            <Link to={"https://www.google.com/"} target='_blank'>    <img src={Logo} alt="Logo" className={styles.logo} /> </Link>
 
             <nav className={styles.menu}>
                 <ul>
-                    <li>Home</li>
-                    <li>Escocia</li>
-                    <li>Grand Canyon</li>
-                    <li>Muralhas da China</li>
-                    <li>Aruba</li>
+                    <li><Link className={styles.navbar}to='/'>Home</Link></li>
+                    <li><Link className={styles.navbar}to='/Escocia'>Escocia</Link></li>
+                    <li><Link className={styles.navbar}to='/Grand'>Grand Canyon</Link></li>
+                    <li><Link className={styles.navbar}to='/Muralhas'>Muralhas da China</Link></li>
+                    <li><Link className={styles.navbar}to='/Aruba'>Aruba</Link></li>
+
 
                 </ul>
             </nav>
